af8b59d8011e94dff391276e3f2d19178f15ce01
Johnson <linghuchongxajh@qq.com>
2025-08-03 09:25:46 +0800

    2025-08-03_09:25

--- backend/char_count/train_grpo.sh
+++ backend/char_count/train_grpo.sh
@@ -38,6 +38,6 @@ python3 -m verl.trainer.main_ppo \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.test_freq=5 \
-    trainer.total_epochs=2 \
+    trainer.total_epochs=1 \
     custom_reward_function.path=./reward_function.py \
     custom_reward_function.name=char_count_reward_function
