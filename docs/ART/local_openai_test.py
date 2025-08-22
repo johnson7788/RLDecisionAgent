a991a50edf63b868e7e02e13e7e90d2250e5fa94
Johnson <linghuchongxajh@qq.com>
2025-08-22 21:08:51 +0800

    2025-08-22_21:08

--- docs/ART/local_openai_test.py
+++ docs/ART/local_openai_test.py
@@ -11,18 +11,32 @@ import asyncio, litellm
 dotenv.load_dotenv()
 litellm._turn_on_debug()
 
+def streaming_main():
+    # openai call
+    response = litellm.completion(
+        model="o4-mini",
+        messages=[{"role":"user","content":"你好"}],
+        base_url="http://localhost:6688",
+        api_key=os.environ["OPENAI_API_KEY"],
+        stream=True
+    )
+
+    for chunk in response:
+        print(chunk)
+
 async def main():
     resp = await litellm.acompletion(
         model="o4-mini",
         messages=[{"role":"user","content":"Hello"}],
         base_url="http://localhost:6688",
-        api_key=os.environ["OPENAI_API_KEY"],
-        max_tokens=50,
+        api_key=os.environ["OPENAI_API_KEY"]
     )
+
     # 正常情况下，LiteLLM 返回 dict（已序列化的对象）
     print(type(resp))
     print(resp)
     print(resp.model_dump())
 
 if __name__ == '__main__':
+    streaming_main()
     asyncio.run(main())
