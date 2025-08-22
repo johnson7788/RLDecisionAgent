cb05aba6a001cbe4b4131eb0cd83623ab58fd864
Johnson <linghuchongxajh@qq.com>
2025-08-22 21:02:32 +0800

    2025-08-22_21:02

---
+++ docs/ART/local_openai_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2025/8/22 20:56
+# @File  : local_openai_test.py
+# @Author: johnson
+# @Contact : github: johnson7788
+# @Desc  : 测试本地的llm代理是否OK
+import os
+import dotenv
+import asyncio, litellm
+dotenv.load_dotenv()
+litellm._turn_on_debug()
+
+async def main():
+    resp = await litellm.acompletion(
+        model="o4-mini",
+        messages=[{"role":"user","content":"Hello"}],
+        base_url="http://localhost:6688",
+        api_key=os.environ["OPENAI_API_KEY"],
+        max_tokens=50,
+    )
+    # 正常情况下，LiteLLM 返回 dict（已序列化的对象）
+    print(type(resp))
+    print(resp)
+    print(resp.model_dump())
+
+if __name__ == '__main__':
+    asyncio.run(main())
