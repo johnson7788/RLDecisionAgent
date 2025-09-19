385b8d25ad9ef091110a1b45641d472ae8a92165
Johnson <linghuchongxajh@qq.com>
2025-09-19 19:48:41 +0800

    2025-09-19_19:48

---
+++ tools/download_vl_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2025/9/19 19:48
+# @File  : download_vl_model.py
+# @Author: johnson
+# @Contact : github: johnson7788
+# @Desc  :  下载VL模型
+
+import torch
+from transformers import AutoTokenizer, AutoModelForImageTextToText
+
+path = "OpenGVLab/InternVL3_5-1B-HF"
+model = AutoModelForImageTextToText.from_pretrained(
+    path,
+    torch_dtype=torch.bfloat16,
+    low_cpu_mem_usage=True,
+    use_flash_attn=True,
+    trust_remote_code=True,
+    device_map="auto").eval()
